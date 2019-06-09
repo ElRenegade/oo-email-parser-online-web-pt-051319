07e36e68679a2b2c1d3e8af922cd954bc612a637
Santiago Sanchez <chagostwo@yahoo.com>
2019-06-09 16:39:08 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,26 +1,13 @@
-# class EmailParser
-#   attr_accessor :emails,
-#
-#   def initialize(emails)
-#     @emails = emails
-#   end
-#   def parse
-#     emails.split.collect do|email|
-#       email.split(",").flatten.uniq
-#     end
-#   end
-# end
 class EmailParser
-attr_accessor :emails
+  attr_accessor :emails,
 
-def initialize(emails)
-  @emails = emails
-end
-
-def parse
-  puts splitEmail = emails.split.collect {|address| address.split(',')}
-  returnArr = splitEmail.flatten.uniq
-  returnArr
-end
-
+  def initialize(emails)
+    @emails = emails
+  end
+  def parse
+    split_emials = emails.split.collect do|email|
+      email.split(",").flatten.uniq
+      split_emials
+    end
+  end
 end
